2564c5f779dc77e1780e146e2316bbf9bee8940d
chronoDave <davidwolters6758@gmail.com>
2022-03-15 17:01:10 +0100

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
-import * as avaritia from './recipes/avaritia';
-import * as exNihilo from './recipes/exNihilo';
-import * as extraUtilites from './recipes/extraUtilities';
-import * as items from './recipes/items';
-import * as nei from './recipes/nei';
-import * as mineFactoryReloaded from './recipes/mineFactoryReloaded';
-import * as thermalexpansion from './recipes/thermalExpansion';
-import * as vanilla from './recipes/vanilla';
-
-export default {
-  avaritia,
-  exNihilo,
-  extraUtilites,
-  items,
-  mineFactoryReloaded,
-  nei,
-  thermalexpansion,
-  vanilla
-};
+export * as avaritia from './recipes/avaritia';
+export * as exNihilo from './recipes/exNihilo';
+export * as extraUtilites from './recipes/extraUtilities';
+export * as items from './recipes/items';
+export * as nei from './recipes/nei';
+export * as mineFactoryReloaded from './recipes/mineFactoryReloaded';
+export * as thermalexpansion from './recipes/thermalExpansion';
+export * as vanilla from './recipes/vanilla';
