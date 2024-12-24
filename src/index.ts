9de7d29a0806d9ce239886f32669849a4472c99a
chronoDave <davidwolters6758@gmail.com>
2024-12-24 18:47:52 +0000

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,161 +1,21 @@
-export {
-  addGrinder,
-  removeGrinder,
-  addInscriber,
-  removeInscriber
-} from './recipes/appliedEnergistics';
-
-export {
-  addCompressor,
-  removeCompressor,
-  addExtreme,
-  removeExtreme
-} from './recipes/avaritia';
-
-export {
-  addComposter,
-  removeComposter,
-  addCrucible as addCrucibleNihilo,
-  removeCrucible as removeCrucibleNihilo,
-  addCrucibleSource,
-  removeCrucibleSource,
-  addHammer,
-  removeHammer,
-  addSieve,
-  removeSieve
-} from './recipes/exNihilo';
-
-export {
-  addQED,
-  removeQED,
-  replaceQED
-} from './recipes/extraUtilities';
-
-export {
-  addCarpenter,
-  removeCarpenter,
-  addCentrifuge,
-  removeCentrifuge,
-  addFermenter,
-  removeFermenter,
-  addFermenterFuel,
-  removeFermenterFuel,
-  removeMoistener,
-  addSqueezer,
-  removeSqueezer,
-  addStill,
-  removeStill,
-  addFabricator,
-  removeFabricator,
-  addFabricatorFuel,
-  removeFabricatorFuel
-} from './recipes/forestry';
-
-export {
-  addDict,
-  removeDict,
-  withName,
-  withTooltip,
-  withTooltipShift,
-  withTag,
-  withEnchantment
-} from './recipes/items';
-
-export {
-  addLaser,
-  removeLaser,
-  addFoci,
-  removeFoci
-} from './recipes/mineFactoryReloaded';
-
-export {
-  hide,
-  addNEI
-} from './recipes/nei';
-
-export {
-  addCrucible as addCrucibleThermal,
-  removeCrucible as removeCrucibleThermal,
-  addFurnace as addFurnaceThermal,
-  removeFurnace as removeFurnaceThermal,
-  addInsolator,
-  removeInsolator,
-  addPulverizer,
-  removePulverizer,
-  addSawmill,
-  removeSawmill,
-  addSmelter,
-  removeSmelter,
-  addTransposerFill,
-  removeTransposerFill,
-  addTransposerExtract,
-  removeTransposerExtract
-} from './recipes/thermalExpansion';
-
-export {
-  add,
-  addMirror,
-  remove,
-  removeShaped,
-  removeShapeless,
-  addFurnace,
-  removeFurnace,
-  replace,
-  replaceAll,
-  replaceMany,
-  addFurnaceFuel,
-  removeFurnaceFuel
-} from './recipes/minecraft';
-
-export {
-  addCastBasin,
-  removeCastBasin,
-  addCastTable,
-  removeCastTable,
-  addDryingRack,
-  removeDryingRack,
-  removeModifier,
-  addSmeltery,
-  removeSmeltery,
-  addSmelteryAlloy,
-  removeSmelteryAlloy,
-  addSmelteryFuel,
-  removeSmelteryFuel,
-  addRepair,
-  removeRepair,
-  setMaterialStats,
-  setMaterialName,
-  setMaterialLevelHarvest,
-  setMaterialDurability,
-  setMaterialSpeed,
-  setMaterialDamage,
-  setMaterialModifierHandle,
-  setMaterialLevelReinforced,
-  setMaterialLevelStonebound,
-  setMaterialStyle,
-  setMaterialAbility,
-  setBowStats,
-  setBowDurability,
-  setBowDrawSpeed,
-  setBowFlightSpeed,
-  setArrowStats,
-  setArrowMass,
-  setArrowBreak,
-  setArrowAccuracy
-} from './recipes/tinkersConstruct';
-
 export type {
-  Ingredient,
   Stack,
+  Ingredient,
+  Bonus,
+  Cast,
   RecipeShaped,
   RecipeShapeless,
-  Recipe,
-  Enchantment,
-  TextRich,
-  Text
+  Recipe
 } from './types';
 
-export {
-  ENCHANTMENTS,
-  MFR_FOCI
-} from './const';
+export * as appliedEnergistics from './recipes/appliedEnergistics';
+export * as avaritia from './recipes/avaritia';
+export * as exNihilo from './recipes/exNihilo';
+export * as extraUtilities from './recipes/extraUtilities';
+export * as forestry from './recipes/forestry';
+export * as item from './recipes/item';
+export * as minecraft from './recipes/minecraft';
+export * as mineFactoryReloaded from './recipes/mineFactoryReloaded';
+export * as nei from './recipes/nei';
+export * as thermalExpansion from './recipes/thermalExpansion';
+export * as tinkersConstruct from './recipes/tinkersConstruct';
